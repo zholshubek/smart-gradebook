0aaec279145b5dd354fd9ab7d064bb904c694e96
zholshubek <zholshubekova@gmail.com>
2026-04-21 19:55:04 +0500

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from reportlab.platypus import Table
 from sklearn.ensemble import RandomForestClassifier
 
 # PDF
